793711fa1604e60ce5497d7c26c5dfb6c3f2dfc1
mmarovich <mmarovich@gmail.com>
2018-08-01 00:07:04 -0500

    added add and delete functionality

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,16 +1,75 @@
 <template>
   <div>
-    <h1>Products</h1>
+
+    <h3>Add Product</h3>
+
+    <div class="card products__add-product">
+    <div>
+      <input
+        v-model="addProductData.product_id"
+        class="validate"
+        placeholder="Id"
+      >
+    </div>
+    <div>
+      <input
+        v-model="addProductData.name"
+        class="validate"
+        placeholder="name"
+      >
+    </div>
+    <div>
+      <input
+        v-model="addProductData.price"
+        class="validate"
+        placeholder="price"
+      >
+    </div>
+    <div>
+      <i 
+        @click="onAdd"
+        class="material-icons medium">add</i>
+    </div>
+    </div>
+
+    <h3>Products</h3>
 
     <div 
       v-for="product in sortedProducts"
       v-bind:key="product.product_id"
       class="collection"
     >
-    <div class="collection-item products_list-item">
+    <div
+      v-if="editId === product.product_id"
+      class="collection-item products__list-item"
+    >
+      <div>{{product.product_id}}</div>
+      <div>
+        <input 
+          class="validate"
+          v-model="editProductData.name"
+        />
+        <input 
+          class="validate"
+          v-model="editProductData.price"
+        />
+      </div>
+    </div>
+    <div 
+      v-else
+      class="collection-item products__list-item"
+    >
       <div>{{product.product_id}}</div>
       <div>{{product.name}}</div>
       <div>{{product.price}}</div>
+      <div>
+        <i 
+        @click="onEdit(product)"
+        class="material-icons">mode_edit</i>
+      <i 
+        @click="onDelete(product.product_id)"
+        class="material-icons">delete</i>
+    </div>
     </div>
 
     </div>
@@ -18,37 +77,82 @@
 </template>
 
 <script>
-import db from '@/db'
+import db from "@/db";
 
 export default {
-  name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      products: []
-    }
+      products: [],
+      addProductData: {
+        product_id: '',
+        name: '',
+        price: ''
+      },
+      editId: '',
+      editProductData: {
+        product_id: '',
+        name: '',
+        price: ''
+      }
+    };
   },
   computed: {
-    sortedProducts () {
+    sortedProducts() {
       return this.products.slice().sort((a, b) => {
-        return a.product_id - b.product_id
-      })
+        return a.product_id - b.product_id;
+      });
     }
   },
-  created () {
-    db.collection('products').get().then(snapshot => {
-      snapshot.forEach(doc => {
-        this.products.push(doc.data())
+  created() {
+    this.getProducts()
+  },
+  methods: {
+    getProducts() {
+      db.collection('products').get().then(snapshot => {
+        const products = []
+        snapshot.forEach(doc => {
+          products.push(doc.data())
+        })
+
+        this.products = products
       })
-    })
+    },
+    onAdd() {
+      db.collection('products').add(this.addProductData)
+        .then(this.getProducts())
+    },
+    onDelete(product_id) {
+      db.collection('products').where('product_id', '==', product_id)
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            doc.ref.delete().then(this.getProducts)
+          })
+        })
+    },
+    onEdit(product) {
+      this.editId = product.product_id
+      this.editProductData = product
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .products_list-item{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+.products__list-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.products__add-product {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.material-icons {
+  cursor: pointer;
+}
 </style>
